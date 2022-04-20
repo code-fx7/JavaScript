7e291a5a32fcac850aee766bcf4595a1679be448
code-fx <code-fx@mail.de>
2022-04-20 11:02:39 +0200

    initial commit

---
+++ apr/20-04/index.js
@@ -0,0 +1,10 @@
+const personObj = {
+  firstName: "Christoph",
+  lastName: "Feliksiak",
+  age: 37,
+  city: "Südbrookmerland",
+  mail: "code-fx@mail.de",
+  kids: ["Ben", "Milian", "Nele"],
+};
+
+console.log(personObj.kids);
