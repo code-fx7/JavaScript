edef85b15b04f6199830d8501840f5b34117056b
code-fx <code-fx@mail.de>
2022-05-02 11:32:59 +0200

    update

---
+++ mai/02-05/index.js
@@ -0,0 +1,46 @@
+// Copy and paste these variables into your JavaScript file. In the commented section, write down which type you think each variable is. Check your answers by printing the types to the console.
+// let length = 16.78; // let lastName = "Johnson"; // let car = ""; // let isOpen = true; //
+
+let length = 16.78;
+let lastName = "Johnson";
+let car = "";
+let isOpen = true;
+
+// commented section
+// let length = 16.78; --> number
+// let lastName = "Johnson"; --> string
+// let car = ""; --> string
+// let isOpen = true; --> boolean
+
+
+console.log(typeof length);
+console.log(typeof lastName);
+console.log(typeof car);
+console.log(typeof isOpen);
+
+
+// Declare two variables called ageMark and ageJohn. Assign their ages - one age should be assigned as a string, the other as a number. Print the variables' data types to the console.
+
+let ageMark = "zweiunddreißig";
+let ageJohn = 42;
+
+console.log(typeof ageMark);
+console.log(typeof ageJohn);
+
+// Is John older than Mark? Declare a variable with a boolean value and print the following to the console eg. "John is older than Mark: true"
+
+// Declare a variable called "str". Do not assign a value. Print str's data type to the console.
+
+// Assign a string to str. Print str's data type again.
+
+// Print the last character of str's string to the console.
+
+// Print the length of str to the console, as well as the data type of str's length.
+
+// Declare three variables: "goals", "penalties", "injuries". Assign numbers to each of the variables. Check if the variable values are equal or not (e.g. console.log(goals == penalties);). Do this for each possibility (goals, penalties) (goals, injuries), (penalties, injuries). Then, print the data type of this check to the console.
+
+// What is the type of Infinity? Comment your answer. Print the type of infinity to the console.
+
+// Declare a variable. Assign the variable the value of a number or a string. Use the isNaN() method to check whether the variable is a number or not. Print "variable is NOT a number: true/false".
+
+// Print the data type of isNaN(variable) to the console.
