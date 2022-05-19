509a1b86935ba312b81b30f476f3b675fdea1a0c
code-fx <code-fx@mail.de>
2022-05-19 15:05:10 +0200

    create api in 19-05

--- mai/19-05/src/index.js
+++ mai/19-05/src/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+const axios = require("axios");
+let [cityName] = process.argv.slice(2);
+let API_KEY = "6270d9087c1c407c91672418221905";
+let baseUrl = "http://api.weatherapi.com/v1";
 
-console.log("Chris");
+let endPoint = `${baseUrl}/current.json?key=${API_KEY}&q=${cityName}`;
+axios
+  .get(endPoint)
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err));
